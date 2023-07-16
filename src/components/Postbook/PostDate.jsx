4515c956a62117f9aea4072f83729dd347ecfdbe
Garfy2 <105547060+Garfy2@users.noreply.github.com>
2023-07-16 18:35:40 +0300

    pages upl

--- src/components/Postbook/PostDate.jsx
+++
@@ -1,5 +0,0 @@
-export const PostDate = ({ date }) => {
-  const formattedDate = new Date(date).toLocaleDateString('ru-RU');
-
-  return <div className="post_date">{formattedDate}</div>;
-};
--- src/contexts/PostsContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-export const PostsContext = createContext({
-  posts: [],
-  error: null,
-  loading: false,
-});
